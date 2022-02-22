fc0cfc6dfd916155e3f1731cc45a7330b8f08d05
Frost1453 <98522906+Frost1453@users.noreply.github.com>
2022-02-22 07:58:25 +0300

    Delete src/me/frost/wizardgame directory

--- src/me/frost/wizardgame/framework/ID.java
+++
@@ -1,9 +0,0 @@
-package me.frost.wizardgame.framework;
-
-public enum ID {
-    Player(),
-    Block(),
-    Chest(),
-    Bullet(),
-    Enemy()
-}
--- src/me/frost/wizardgame/game/Launcher.java
+++
@@ -1,7 +0,0 @@
-package me.frost.wizardgame.game;
-
-public class Launcher {
-    public static void main(String[] args) {
-        new Game();
-    }
-}
